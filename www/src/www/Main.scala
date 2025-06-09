d66afb1c2b064f506da22f01e3bcb11ee4156010
nguyenyou <tunt081295@gmail.com>
2025-06-10 00:35:59 +0700

    🚢 ship it

--- www/src/www/Main.scala
+++ www/src/www/Main.scala
@@ -3,7 +3,11 @@ package www
 import org.scalajs.dom
 import com.raquo.laminar.api.L.*
 
-@main 
-def main(): Unit = {
+object Main {
+  @main
+  def run(): Unit = {
     render(dom.document.getElementById("app"), div("Mill + Vite + Scala.js"))
+  }
+
+  def hello(): String = "Hello World"
 }
