ec4aff238ec0b1cc96b17d3be0d4ed3f80b9431b
nguyenyou <tunt081295@gmail.com>
2025-09-29 14:13:49 +0700

    🚢 ship it

--- www/src/www/ReactMain.scala
+++ www/src/www/ReactMain.scala
@@ -7,15 +7,5 @@ import japgolly.scalajs.react.vdom.html_<^.*
 
 def MainReactApp(): Unit = {
   val container = dom.document.getElementById("app")
-
-  val Component = ScalaFnComponent
-    .withHooks[Unit]
-    .useState(0)
-    .render((props, count) => {
-      <.div(
-        "React Hello"
-      )
-    })
-
-  ReactDOMClient.createRoot(container).render(Component())
+  ReactDOMClient.createRoot(container).render(ReactApp()())
 }
