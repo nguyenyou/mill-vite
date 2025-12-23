1359e6f7de77471473d610a495066b56d7e9a57f
nguyenyou <tunt081295@gmail.com>
2025-12-23 22:01:07 +0700

    hook

--- www/src/www/ReactApp.scala
+++ www/src/www/ReactApp.scala
@@ -2,10 +2,11 @@ package www
 
 import japgolly.scalajs.react.*
 import japgolly.scalajs.react.vdom.html_<^.*
+import org.scalajs.dom.document
 
 case class ReactApp() {
   def apply(): VdomElement = {
-    ReactApp.component(this)
+    ReactApp.component()
   }
 }
 
@@ -18,8 +19,20 @@ object ReactApp {
     }
   }
 
-  val component = ScalaComponent
-    .builder[Props](getClass.getSimpleName)
-    .renderBackend[Backend]
-    .build
+  val component = ScalaFnComponent[Unit](props =>
+    for {
+      count <- useState(0)
+      _ <- useEffect(Callback {
+        document.title = s"You clicked ${count.value} times"
+      })
+      fruit <- useState("banana")
+    } yield <.div(
+      <.p(s"You clicked ${count.value} times"),
+      <.button(
+        ^.onClick --> count.modState(_ + 1),
+        "Click me"
+      ),
+      <.p(s"Your favourite fruit is a ${fruit.value}!")
+    )
+  )
 }
